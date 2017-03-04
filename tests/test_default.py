b11925726ebf2a17fecf40e953911926df9fd216
Vinícius Figueiredo <viniciusfs@gmail.com>
2017-03-04 16:37:06 -0300

    Added testinfra tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,9 +4,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(File):
-    f = File('/etc/hosts')
+def test_firewalld_package(Package):
+    firewalld = Package('firewalld')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert firewalld.is_installed
+
+
+def test_firewall_service(Service):
+    firewalld = Service('firewalld')
+
+    assert firewalld.is_running
+    assert firewalld.is_enabled
